9754bde56086238a0b2ba33e0057b4c36460bf7c
Luis Arpasi <luiggi@Luiss-MacBook-Pro.local>
2018-11-07 00:19:15 -0500

    Add help to add new aliases in webpack

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+// For extra aliases you can use chanWebpack
+// const path = require('path')
+
 module.exports = {
   productionSourceMap: false,
-  lintOnSave: false
+  lintOnSave: false,
+  // chainWebpack: config => {
+  //   config.resolve.alias.set('folder1', path.resolve(__dirname, 'src/folder1'))
+  // }
 }
